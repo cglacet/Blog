789021c8e853a5986a680d5146afb86f2880708e
Christian Glacet <christian.glacet@gmail.com>
2019-02-28 23:00:38 +0100

    publish script update

--- python/publish.sh
+++ python/publish.sh
@@ -1,18 +1,18 @@
 if [ "$#" -le 0 ]; then
    echo "Error: you have to provide a commit message to publish."
-   exit 1
+   exit 128
 fi
 
 commitmsg="$*"
 
 if [ ${#commitmsg} -lt 5 ]; then
    echo "Error: commit message should be at least 5 characters long."
-   exit 1
+   exit 128
 fi
 
 jupyter nbconvert --stdout --to markdown notes.ipynb > README.md
 git add notes.ipynb README.md
-git commit -m \"$commitmsg\"
+git commit -m "$commitmsg"
 git push origin master
 
 exit 0
